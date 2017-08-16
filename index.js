18aef246d64b1bc50323f694fe7e0704b0fa1d4f
sbiger <superbiger@qq.com>
2017-08-16 15:47:25 +0800

    fix：

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
-var PinyinFormat = require('./libs/PinyinHelper');
 var PinyinHelper = require('./libs/PinyinHelper');
 var ChineseHelper = require('./libs/ChineseHelper');
 ;(function(){
-    global.PinyinFormat = PinyinFormat.PinyinFormat;
     global.PinyinHelper = PinyinHelper.PinyinHelper;
     global.ChineseHelper = ChineseHelper.ChineseHelper;
 })();
